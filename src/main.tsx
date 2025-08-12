300ebf4be2f319568f482ee4084ffcecc9f3d869
Saif ali khan <asaif5171@gmail.com>
2025-08-12 19:24:06 +0530

    path not show

--- src/main.tsx
+++ src/main.tsx
@@ -1,42 +1,21 @@
-// import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
-// import './index.css'
-// import Navbar from './Navbar.tsx'
-// import Footer from './Footer.tsx'
-// import KycAndCredit from './CashandCredit.tsx'
-
-
-
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <Navbar/>
-
-//    <KycAndCredit/>
-
-//   <Footer/>
-//   </StrictMode>,
-// )
-
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { MemoryRouter, Routes, Route, } from 'react-router-dom';
 import './index.css';
 import Navbar from './Navbar.tsx';
 import Footer from './Footer.tsx';
 import KycAndCredit from './CashandCredit.tsx';
 import CreditCustomers from './Creditkyc.tsx';
 
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<KycAndCredit />} />
-        <Route path="/credit-customers" element={<CreditCustomers/>} />
+        <Route path="/credit-customers" element={<CreditCustomers />} />
       </Routes>
       <Footer />
-    </BrowserRouter>
+    </MemoryRouter>
   </StrictMode>
 );
-
